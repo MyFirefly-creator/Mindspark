2bcb6865072bcd7c101c8fe875e9e48140062556
MyFirefly-creator <bdiwuputra@gmail.com>
2025-02-10 13:26:44 +0700

    Perbaikan dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,7 +1,5 @@
 @extends('component.app')
 
-@section('title', 'Beranda')
-
 @section('content')
     <div class="container">
         <div class="d-flex justify-content-between align-items-center mb-3">
